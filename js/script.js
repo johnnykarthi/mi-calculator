c35e895a48d5aee0b1e80ac7311ca9603b8551f8
Karthick V <72920492+johnnykarthi@users.noreply.github.com>
2023-01-03 10:00:49 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,59 @@
 let output = document.querySelector(".output");
-let result= '';
-function display(num){
-    result = result+num;
-    output.innerHTML = result;
+let result = '';
+let c = 0;
+function display(num) {
+    let n = result.length;
+    if ((num == '+' || num == '-' || num == '*' || num == '/' || num == '%') && (c == 1)) {
+        c=0;
+        result = result+num;
+        output.innerHTML = result;
+    }
+    else if (c == 1) {
+        c=0;
+        result='';
+        result=result+num;
+        output.innerHTML = result;
+    }
+    else if (n > 0) {
+        let last = result.charAt(n-1);
+        if ((last == '+' || last == '-' || last == '*' || last == '/' || last == '%') && (num == '+' || num == '-' || num == '*' || num == '/' || num == '%')) {
+            output.innerHTML = result;
+        }
+        else{
+            result = result + num;
+            output.innerHTML = result;
+        }
+    }
+    else {
+        result = result + num;
+        output.innerHTML = result;
+    }
 }
 
-function sk(){
+function sk() {
     output.innerHTML = 0;
-    result='';
+    result = '';
 }
 
-function del(){
-    result = result.slice(0,-1);
+function del() {
+    result = result.slice(0, -1);
     output.innerHTML = result;
-    if(result == '')
-    {
+    if (result == '') {
         output.innerHTML = 0;
     }
 }
 
-function cal(){
-    result = eval(result);
-    output.innerHTML ="= "+result;
+function cal() {
+    if(result!='')
+    {
+        result = ''+eval(result);
+        output.innerHTML = "= " + result;
+        c=1;
+    }
+    else
+    {
+        output.innerHTML = "= " + '0';
+    }
+   
 
 }
